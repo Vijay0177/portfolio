499ef9e01efeaa8fc7be17102749ed3b9ca8cbd2
Vijay0177 <vijayr.hcl@gmail.com>
2025-11-22 18:03:53 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,41 @@
+const navLinks = [
+	{
+		id: 1,
+		name: 'Portfolio',
+		type: 'finder',
+	},
+	{
+		id: 2,
+		name: 'Contact',
+		type: 'contact',
+	},
+	{
+		id: 3,
+		name: 'Projects',
+		type: 'projects',
+	},
+]
+
+const navIcons = [
+	{
+		id: 1,
+		img: '/icons/wifi.svg',
+	},
+	{
+		id: 2,
+		img: '/icons/search.svg',
+	},
+	{
+		id: 3,
+		img: '/icons/user.svg',
+	},
+	{
+		id: 4,
+		img: '/icons/mode.svg',
+	},
+]
+
+export {
+	navLinks,
+	navIcons,
+}
