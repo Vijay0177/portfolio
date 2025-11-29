e0b7114743a8b67499f2c8fb258f57e77d519f2b
Vijay0177 <vijayr.hcl@gmail.com>
2025-11-29 17:19:12 +0530

    portfolio code finalize

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Dock, Navbar, Welcome } from "#components"
-import { Resume, Safari, Terminal } from "#windows"
+import { Dock, Home, Navbar, Welcome } from "#components"
+import { Contact, Finder, Image, Resume, Safari, Terminal, Text } from "#windows"
 import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
 
@@ -14,6 +14,11 @@ function App() {
 			<Terminal />
 			<Safari />
 			<Resume />
+			<Finder />
+			<Text />
+			<Image />
+			<Contact />
+			<Home />
 		</main>
 	)
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -126,7 +126,22 @@ const techStack = [
 	},
 ]
 
-const socials = []
+const socials = [
+	{
+		id: 1,
+		text: 'GitHub',
+		icon: '/icons/github.svg',
+		bg: '#f4656b',
+		link: 'https://github.com/Vijay0177',
+	},
+	{
+		id: 2,
+		text: 'LinkedIn',
+		icon: '/icons/linkedin.svg',
+		bg: '#05b6f6',
+		link: 'https://www.linkedin.com/in/vijayrawat-flutter-developer/',
+	},
+]
 
 const photosLinks = []
 
@@ -141,6 +156,132 @@ export {
 	socials,
 	photosLinks,
 	gallery,
+}
+
+const WORK_LOCATION = {
+	id: 1,
+	type: 'work',
+	name: 'Work',
+	icon: '/icons/work.svg',
+	kind: 'folder',
+	children: [
+		{
+			id: 5,
+			name: 'ProgrammerCube',
+			icon: '/images/folder.png',
+			kind: 'folder',
+			position: 'top-10 left-5',
+			windowPosition: 'top-[5vh] left-5',
+			children: [],
+		}
+	],
+}
+
+const ABOUT_LOCATION = {
+	id: 2,
+	type: "about",
+	name: "About me",
+	icon: "/icons/info.svg",
+	kind: "folder",
+	children: [
+		{
+			id: 1,
+			name: "me.png",
+			icon: "/images/image.png",
+			kind: "file",
+			fileType: "img",
+			position: "top-10 left-5",
+			imageUrl: "/images/adrian.jpg",
+		},
+		{
+			id: 2,
+			name: "casual-me.png",
+			icon: "/images/image.png",
+			kind: "file",
+			fileType: "img",
+			position: "top-28 right-72",
+			imageUrl: "/images/adrian-2.jpg",
+		},
+		{
+			id: 3,
+			name: "conference-me.png",
+			icon: "/images/image.png",
+			kind: "file",
+			fileType: "img",
+			position: "top-52 left-80",
+			imageUrl: "/images/adrian-3.jpeg",
+		},
+		{
+			id: 4,
+			name: "about-me.txt",
+			icon: "/images/txt.png",
+			kind: "file",
+			fileType: "txt",
+			position: "top-60 left-5",
+			subtitle: "Meet the Developer Behind the Code",
+			image: "/images/adrian.jpg",
+			description: [
+				"Hey! I’m Adrian 👋, a web developer who enjoys building sleek, interactive websites that actually work well.",
+				"I specialize in JavaScript, React, and Next.js—and I love making things feel smooth, fast, and just a little bit delightful.",
+				"I’m big on clean UI, good UX, and writing code that doesn’t need a search party to debug.",
+				"Outside of dev work, you'll find me tweaking layouts at 2AM, sipping overpriced coffee, or impulse-buying gadgets I absolutely convinced myself I needed 😅",
+			],
+		},
+	],
+}
+
+const RESUME_LOCATION = {
+	id: 3,
+	type: "resume",
+	name: "Resume",
+	icon: "/icons/file.svg",
+	kind: "folder",
+	children: [
+		{
+			id: 1,
+			name: "Resume.pdf",
+			icon: "/images/pdf.png",
+			kind: "file",
+			fileType: "pdf",
+			// you can add `href` if you want to open a hosted resume
+			// href: "/your/resume/path.pdf",
+		},
+	],
+}
+
+const TRASH_LOCATION = {
+	id: 4,
+	type: "trash",
+	name: "Trash",
+	icon: "/icons/trash.svg",
+	kind: "folder",
+	children: [
+		{
+			id: 1,
+			name: "trash1.png",
+			icon: "/images/image.png",
+			kind: "file",
+			fileType: "img",
+			position: "top-10 left-10",
+			imageUrl: "/images/trash-1.png",
+		},
+		{
+			id: 2,
+			name: "trash2.png",
+			icon: "/images/image.png",
+			kind: "file",
+			fileType: "img",
+			position: "top-40 left-80",
+			imageUrl: "/images/trash-2.png",
+		},
+	],
+}
+
+export const locations = {
+	work: WORK_LOCATION,
+	about: ABOUT_LOCATION,
+	resume: RESUME_LOCATION,
+	trash: TRASH_LOCATION,
 }
 
 const INITIAL_Z_INDEX = 1000
@@ -176,7 +317,7 @@ const WINDOW_CONFIG = {
 		zIndex: INITIAL_Z_INDEX,
 		data: null,
 	},
-	txtfile: {
+	textfile: {
 		isOpen: false,
 		zIndex: INITIAL_Z_INDEX,
 		data: null,
